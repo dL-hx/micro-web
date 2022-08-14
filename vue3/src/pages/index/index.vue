f3d4d9a100b64f695c35d14f3651a8491582fc1b
dL-hx <897057246@qq.com>
2022-08-15 00:16:47 +0800

    vue3 初始化

---
+++ vue3/src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'index',
+  components: {
+ 
+  },
+  setup() {
+    return {
+       
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
