18b8b387eac04fb0830a963b64c4cd013c320a3f
dL-hx <897057246@qq.com>
2022-08-20 12:46:33 +0800

    feat: 导入之前的项目配置(完成react16

--- react15/index.js
+++ react15/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import BasicMap from './src/router/index.jsx';
-import BasicMap from './src/router/index1.jsx';
+import BasicMap from './src/router/index.jsx';
+// import BasicMap from './src/router/index1.jsx';
 import "./index.scss"
 
 const render = () => {
